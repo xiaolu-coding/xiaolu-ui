f2f92961ea750e451427cbc556ecc5fe87d083b7
xiaolu-coding <454819375@qq.com>
2022-07-27 14:53:26 +0800

    chore: prettier

--- xiaolu-ui/src/App.vue
+++ xiaolu-ui/src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test'
+import Test from "./components/Test";
 </script>
 
 <template>
