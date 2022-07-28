22609d2f24ea08f134fc2f949d0661ece46e6357
xiaolu-coding <454819375@qq.com>
2022-07-28 19:46:45 +0800

    chore: 引入taliwindcss

--- xiaolu-ui/src/App.vue
+++ xiaolu-ui/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 import Test from "./components/Test";
 </script>
 
@@ -15,7 +15,7 @@ import Test from "./components/Test";
     </a>
   </div> -->
   <Test />
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
