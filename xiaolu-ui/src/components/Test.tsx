62963f6e398c4fc120657e0778b63c141d6914ec
xiaolu-coding <454819375@qq.com>
2022-07-28 20:41:05 +0800

    chore: tsx写法

--- xiaolu-ui/src/components/Test.tsx
+++ xiaolu-ui/src/components/Test.tsx
@@ -1,1 +1,13 @@
-export default () => <div>test</div>;
+import { defineComponent } from "vue";
+
+// export default () => <div>test</div>;
+
+// 控制力最强的vue3 tsx写法
+// 摒弃了this
+export default defineComponent({
+  setup(props, ctx) {
+    return () => {
+      return <div>test1</div>
+    }
+  }
+})
